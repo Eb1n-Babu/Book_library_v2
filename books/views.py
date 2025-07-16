956d92231524812bb0fdb646fa10673c81352d2a
Eb1n-Babu <ebin.babu.in@icloud.com>
2025-07-16 14:14:10 +0530

    Implemented class-based view and function-based view for study purpose

--- books/views.py
+++ books/views.py
@@ -1,7 +1,25 @@
-from rest_framework import generics
+from rest_framework import status, generics
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from .models import Book
 from .serializers import BookSerializer
 
-class BookListCreateAPIView(generics.ListCreateAPIView):
+@api_view(['GET', 'POST'])
+def book_list(request):
+    if request.method == 'GET':
+        books = Book.objects.all()
+        serializer = BookSerializer(books, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = BookSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    return None
+
+
+class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
